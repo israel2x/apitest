27e705727961ad3ebe54940cf0b70ca30ae73fd1
israel2x <ezurita@fiec.espol.edu.ec>
2025-05-12 19:49:17 -0500

    clase 9 de mayo, uso del API del clima

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,8 @@ class _DatosClimaState extends State<DatosClima> {
   String ciudad = "";
   String temp = "";
   String clima = "";
+  double temp2 = 0;
+
   List posts = [];
 
   void getClima() {
@@ -54,18 +56,45 @@ class _DatosClimaState extends State<DatosClima> {
     }
   }
 
+  double convertirKaC(double dato) {
+    return dato - 273.15;
+  }
+
+  Future<void> getDataClima() async {
+    final url = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?lat=-2.3348&lon=-79.395221&appid=1af6229b9e9cae6b78bca3cd9006f199");
+    final response = await http.get(url);
+
+    print(response);
+
+    if (response.statusCode == 200) {
+      print(jsonDecode(response.body));
+
+      var data = jsonDecode(response.body);
+
+      setState(() {
+        ciudad = data['name'];
+        temp2 = convertirKaC(data['main']['temp']);
+      });
+
+      print(ciudad);
+      print(temp2);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Text("Datos:"),
         Text("Ciudad:${ciudad}"),
-        Text("Temperatura:${temp}"),
+        Text("Temperatura:${temp2}"),
         Text("Clima:${clima}"),
         TextButton(
             onPressed: () {
-              getClima();
-              getDataAPI();
+              //getClima();
+              //getDataAPI();
+              getDataClima();
             },
             child: Text("Mostrar Clima"))
       ],
